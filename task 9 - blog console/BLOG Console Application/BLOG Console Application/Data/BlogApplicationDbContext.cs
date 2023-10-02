885f5b01393b0bfeaab281b51c63157b394da081
Johnson <johnsondawodu22@gmail.com>
2023-10-02 15:08:54 +0100

    add ef core task

--- task 9 - blog console/BLOG Console Application/BLOG Console Application/Data/BlogApplicationDbContext.cs
+++ task 9 - blog console/BLOG Console Application/BLOG Console Application/Data/BlogApplicationDbContext.cs
@@ -13,37 +13,5 @@ namespace BLOG_Console_Application.Data
         public DbSet<Post> Posts { get; set; }
         public DbSet<Comment> Comments { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-
-            modelBuilder.Entity<Comment>(
-                entity => {
-                    entity.HasOne(e => e.Post)
-                        .WithMany(e => e.Comments)
-                        .HasForeignKey(e => e.PostId)
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .HasConstraintName("FK_Comment_Post");
-                }
-            );
-
-            modelBuilder.Entity<Comment>()
-                .Property(c => c.CreatedAt)
-                .HasDefaultValueSql("CURRENT_TIMESTAMP AT TIME ZONE 'UTC'");
-
-            modelBuilder.Entity<Comment>()
-                .Property(p => p.CommentId)
-                .UseIdentityColumn();
-
-            modelBuilder.Entity<Post>()
-                .Property(p => p.PostId)
-                .UseIdentityColumn();
-
-            modelBuilder.Entity<Post>()
-                .Property(c => c.CreatedAt)
-                .HasDefaultValueSql("CURRENT_TIMESTAMP AT TIME ZONE 'UTC'");
-
-
-        }
-
     }
 }
